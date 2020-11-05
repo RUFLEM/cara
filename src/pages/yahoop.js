8e5714ae92c5824a3b2ddd1380f319f720ae6537
Rupert Fleming <rupertfleming@gmail.com>
2020-11-05 17:19:44 +0000

    add Yahoop boilerplate

---
+++ src/pages/yahoop.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+export default function Yahoop() {
+    return (
+        <div>
+            <div className="Yahoop">
+            </div >
+            <div style={{display: "flex"}}>
+            <a  href="/project">Projects</a>
+            </div>
+        </div>
+    );
+}
